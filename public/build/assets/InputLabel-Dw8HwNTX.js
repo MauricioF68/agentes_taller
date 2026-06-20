663985290a15b336aedf8191333a78d0c279e427
MauricioF68 <mauricioterrones98@gmail.com>
2026-06-20 09:53:23 -0500

    build: update production assets and manifest for new components and layout features

--- public/build/assets/InputLabel-Dw8HwNTX.js
+++
@@ -1,1 +0,0 @@
-import{j as m}from"./app-Dv-u3dMP.js";function o({value:t,className:e="",children:r,...s}){return m.jsx("label",{...s,className:"block text-sm font-medium text-gray-700 "+e,children:t||r})}export{o as I};
