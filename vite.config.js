9116446a3361da3b11e07853f840b1162a001f7f
MauricioF68 <mauricioterrones98@gmail.com>
2026-06-20 10:08:40 -0500

    fix: update production base path in vite config to root directory

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ command }) => ({
     // Si ejecutas 'npm run build', usará la ruta de Hostinger.
     // Si ejecutas 'npm run dev', usará la ruta local.
-    base: command === 'build' ? '/agentes/public/build/' : '/',
+    base: command === 'build' ? '/build/' : '/',
     plugins: [
         laravel({
             input: 'resources/js/app.jsx',
