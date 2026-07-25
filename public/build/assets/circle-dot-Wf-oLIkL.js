1ac38a71f865fdf40c2ef5d1a71eb88a8679917c
MauricioF68 <mauricioterrones98@gmail.com>
2026-07-25 12:23:29 -0500

    feat: implement build assets and motion components for dashboard and interface elements

--- public/build/assets/circle-dot-Wf-oLIkL.js
+++
@@ -1,1 +0,0 @@
-import{c}from"./createLucideIcon-Dj7fT37a.js";const e=[["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}],["circle",{cx:"12",cy:"12",r:"1",key:"41hilf"}]],r=c("circle-dot",e);export{r as C};
